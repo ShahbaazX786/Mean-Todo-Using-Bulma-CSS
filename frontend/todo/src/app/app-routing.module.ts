5c49a61be7fae48ecbe5eb4bf1ea68262171416e
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-04 10:29:00 +0530

    added routing paths

--- frontend/todo/src/app/app-routing.module.ts
+++ frontend/todo/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { HowToComponent } from './pages/how-to/how-to.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent },
+  {path:'howto', component: HowToComponent },
+  {path:'navbar', component: NavbarComponent }
+  {path:'footer', component: FooterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- frontend/todo/src/app/app.module.ts
+++ frontend/todo/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { HowToComponent } from './pages/how-to/how-to.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HowToComponent,
+    NavbarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
