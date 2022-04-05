c88f31081e7f9176d4a16e5958c373c3f9d235e4
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-05 12:34:36 +0530

    APIS created and tested with postman

--- backend/app.js
+++ backend/app.js
@@ -11,6 +11,7 @@ app.use(bodyParser.json());
 const List=require('./db/models/listmodel');
 const Task=require('./db/models/taskmodel');
 
+//APIs for LISTS
 app.get('/lists',(req,res)=>{
     List.find({}).then((lists)=>{
         res.send(lists);
@@ -24,6 +25,50 @@ app.post('/lists',(req,res)=>{
         res.send(listDoc);
     })
 });
+
+app.patch('/lists/:id',(req,res)=>{
+    List.findByIdAndUpdate({ _id: req.params.id },{$set:req.body}).then(()=>{
+        res.sendStatus(200);
+    });
+});
+
+
+app.delete('/lists/:id',(req,res)=>{
+    List.findByIdAndRemove({ _id: req.params.id }).then((removedListDoc)=>{
+        res.send(removedListDoc);
+    });
+});
+
+//APIS for TASKS
+app.get('/lists/:listid/tasks',(req,res)=>{
+    Task.find({_listId:req.params.listid}).then((tasks)=>{
+        res.send(tasks);
+    });
+})
+
+app.post('/lists/:listid/tasks',(req,res)=>{
+    let newTask=new Task({
+        title:req.body.title,
+        _listId:req.params.listid
+    })
+    newTask.save().then((taskDoc)=>{
+        res.send(taskDoc);
+    })
+});
+
+app.patch('/lists/:listid/tasks/:taskid',(req,res)=>{
+    Task.findByIdAndUpdate({ _id: req.params.taskid,_listId:req.params.listid},{$set:req.body}).then(()=>{
+        res.sendStatus(200);
+    });
+});
+
+
+app.delete('/lists/:listid/tasks/:taskid',(req,res)=>{
+    Task.findByIdAndRemove({ _id: req.params.taskid,_listId:req.params.listid }).then((removedTaskDoc)=>{
+        res.send(removedTaskDoc);
+    });
+});
+
 
 app.listen(3000, ()=>{
     console.log("server is listening on port 3000");
