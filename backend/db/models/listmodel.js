5e8adb4343608d21c21346f882c730d0c3b446f3
Shaik Shahbaaz Alam <Shahbaazplatinum@gmail.com>
2022-04-11 12:25:06 +0530

    ignoreable changes

--- backend/db/models/listmodel.js
+++ backend/db/models/listmodel.js
@@ -2,9 +2,9 @@ const mongoose = require('mongoose');
 
 const ListSchema=new mongoose.Schema({
     title: {
-        type:String,
-        required:true,
-        minlength:1
+        type: String,
+        required: true,
+        minlength: 1
     }
 })
 
--- backend/db/models/taskmodel.js
+++ backend/db/models/taskmodel.js
@@ -2,9 +2,10 @@ const mongoose = require('mongoose');
 
 const TaskSchema=new mongoose.Schema({
     title: {
-        type:String,
-        required:true,
-        minlength:1
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
     },
     _listId: {
         type:mongoose.Types.ObjectId,
